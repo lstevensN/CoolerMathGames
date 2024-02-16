388afeceff1654dca54e54be141acda040432e79
JohnBeck19 <jbeck@student.neumont.edu>
2024-02-15 18:37:11 -0700

    City escape (webGL build)

---
+++ CoolerMathGames/Controllers/CityEscapeController.cs
@@ -0,0 +1,28 @@
+﻿using CoolerMathGames.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoolerMathGames.Controllers
+{
+    public class CityEscapeController : Controller
+    {
+        private readonly ILogger<CityEscapeController> _logger;
+
+        public CityEscapeController(ILogger<CityEscapeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CityEscape()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
