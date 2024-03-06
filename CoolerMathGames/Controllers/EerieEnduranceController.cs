5b88b09820aeb3a4ee6ec3298498d0522dcbc989
JohnBeck19 <jbeck@student.neumont.edu>
2024-03-05 18:35:15 -0700

    Game views and controllers

---
+++ CoolerMathGames/Controllers/EerieEnduranceController.cs
@@ -0,0 +1,28 @@
+﻿using CoolerMathGames.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoolerMathGames.Controllers
+{
+    public class EerieEnduranceController : Controller
+    {
+        private readonly ILogger<EerieEnduranceController> _logger;
+
+        public EerieEnduranceController(ILogger<EerieEnduranceController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult EerieEndurance()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoolerMathGames/Controllers/GalaxyGunshipController.cs
@@ -0,0 +1,28 @@
+﻿using CoolerMathGames.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoolerMathGames.Controllers
+{
+    public class GalaxyGunshipController : Controller
+    {
+        private readonly ILogger<GalaxyGunshipController> _logger;
+
+        public GalaxyGunshipController(ILogger<GalaxyGunshipController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult GalaxyGunship()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
